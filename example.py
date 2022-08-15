8b44343b63179513c8a8ad2e4762df09fe1c3964
Kenneth Jeong <kenneth.jeong@proton.me>
2022-08-15 12:00:00 +0900

    style: pep 8

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 class Empty(Exception):
     pass
 
+
 class ArrayQueue:
 
     def __init__(self):
         self._data = []
-
 
     def __len__(self):
         return len(self._data)
@@ -16,31 +16,31 @@ class ArrayQueue:
         else:
             return True
 
-
-  def first(self):
-        if len(self._data) == False:
+    def first(self):
+        if len(self._data) is False:
             raise Empty("error")
         else:
             return self._data[0]
 
     def dequeue(self):
-        if len(self._data) == False:
+        if len(self._data) is False:
             raise Empty("error")
 
         val = self._data[0]
         del self._data[0]
         return val
 
-
     def enqueue(self, e):
         self._data.append(e)
 
 
 Q = ArrayQueue()
-Q.enqueue(5)
-Q.enqueue(3)
-print(len(Q))
-print(Q.dequeue())
-print(len(Q))
-print(Q.first())
 
+Q.enqueue(5)        # Q._data 리스트에 5 추가(append), Q._data = [5]
+Q.enqueue(3)        # Q._data 리스트에 3 추가(append), Q._data = [5, 3]
+print(len(Q))       # Q._data 리스트의 길이인 2 출력
+
+print(Q.dequeue())  # Q._data 리스트에서 인덱스가 0인 값 5를 삭제하고 출력, Q._data = [3]
+print(len(Q))       # Q._data 리스트의 길이인 1 출력
+
+print(Q.first())    # Q._data 리스트에서 인덱스가 0인 값 3을 출력
